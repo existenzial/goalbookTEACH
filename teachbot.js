139edde350a14c30b97ad273cbce1a2623fd1142
Jazz Lyles <ripley36706@gmail.com>
2016-11-22 13:19:48 -0800

    code cleanup, renamed helper func expressions

--- teachbot.js
+++ teachbot.js
@@ -4,9 +4,6 @@ const { pandoraOptions, twitOptions } = require('./credentials');
 
 const bot = new Pandorabot( pandoraOptions );
 const T = new Twit( twitOptions );
-const goalbookTEACHUrl = 'direct_messages/new';
-
-let stream = T.stream('user', { stringify_friend_ids: true });
 
 // Pandorabot Params
 const talkParams = {
@@ -19,7 +16,6 @@ const talkParams = {
 
 // GET to 'search/tweets' for Top 10 Goalbook Tweets
 let topTenGoalbookTweets = { q: 'goalbook', count: 10 };
-
 const getGoalbookTweets = (err, data, res) => {
   let tweets = data.statuses;
   for (let i = 0; i < tweets.length; i++) {
@@ -27,20 +23,25 @@ const getGoalbookTweets = (err, data, res) => {
     console.log( tweet );
   }
 }
+const getTopTenTweets = () => {
+  T.get( 'search/tweets', topTenGoalbookTweets, getGoalbookTweets );
+}
 
-//T.get( 'search/tweets', topTenGoalbookTweets, getGoalbookTweets );
-
+// GET to 'direct_messages' to use most recent DM as bot input
 let goalbookTEACHDMParams = { include_entities: false, skip_statuses: true };
-
-const getGoalbookDMs = (err, data, res) => {
+const goalbookDMBotTalk = (err, data, res) => {
   let messages = data;
   let mostRecentMessage = messages[0].text;
   talkParams.input = mostRecentMessage;
 
-  console.log( talkParams.input );
+  console.log( talkParams.input ); //user input
   bot.talk( talkParams, (err, res) => {
-    if (!err) { console.log( res.responses[0] ); }
+    if (!err) { console.log( res.responses[0] ); } //bot response to user input
   });
 }
 
-T.get( 'direct_messages', goalbookTEACHDMParams, getGoalbookDMs );
+const pollDMs = () => {
+  T.get( 'direct_messages', goalbookTEACHDMParams, goalbookDMBotTalk );
+}
+
+pollDMs();
